ba029791e910bcce02ff8da616fa9fd2c2da91d7
632114157qq.com <632114157@qq.com>
2019-02-18 19:21:32 +0800

    添加登陆界面

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Login from '@/components/login'
+import Login from '@/views/login/login'
 import Index from '@/components/index'
 Vue.use(Router)
 
